b13960225ee9fc3587a25720e88d2c531d131c0c
ironaddicteddog <ironaddicteddog@gmail.com>
2024-01-14 16:33:41 +0000

    Clean escrow.rs

--- programs/anchor-escrow/src/states/escrow.rs
+++ programs/anchor-escrow/src/states/escrow.rs
@@ -15,18 +15,3 @@ impl Space for Escrow {
     // First 8 Bytes are Discriminator (u64)
     const INIT_SPACE: usize = 8 + 8 + 1 + 32 + 32 + 32 + 8 + 8;
 }
-
-// impl EscrowState {
-//     pub fn space() -> usize {
-//         8 + 121
-//     }
-// }
-
-// #[account]
-// pub struct Escrow {
-//     pub seed: u64,
-//     pub mint_a: Pubkey,
-//     pub mint_b: Pubkey,
-//     pub receive: u64,
-//     pub bump: u8,
-// }
